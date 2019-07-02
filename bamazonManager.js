87c7b9b7ec3a8edcfd46aff722fdd22f52747b73
Triumph <archie200@icloud.com>
2019-07-02 08:50:05 -0500

    Adding functionality

--- bamazonManager.js
+++ bamazonManager.js
@@ -13,5 +13,88 @@ var connection = mysql.createConnection({
 connection.connect(function(error){
     if (error) throw error;
     console.log("connected as id " + connection.threadId + "\n");
-    readData();
+    // readData();
 });
+
+function displayMenu(){
+    
+    inquirer
+  .prompt({
+    /* Pass your questions in here */
+    
+    type: "list",
+    name: "choice",
+    message: "What would you like to do?\n",
+    choices: ["Buy an item", "Add an item into inventory", "Exit"]
+    
+    })
+  .then(function (answer) {
+    switch (answer.choice){
+        case "Buy an item":
+            buyItem();
+            break;
+        case "Add an item into inventory":
+            addItem();
+            break;
+        case "Exit":
+            exitProgram();
+    }
+    });
+};
+displayInventory();
+displayMenu();
+
+function buyItem(){
+
+};
+
+function addItem(){
+    console.log("Please enter the product\n");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "item",
+            message: "Please enter a product name: "
+        }
+    ])
+    .then(function (answer) {});
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "department",
+            message: "Select a department",
+            choice: ["Electronics", "Games", "Furniture"]
+        }
+    ])
+    .then(function (answer) {});
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "price",
+            message: "Enter the price"
+        }
+    ])
+    .then(function (answer) {});
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "quantity",
+            message: "Enter the quantity"
+        }
+    ])
+    .then(function (answer) {});
+};
+
+function exitProgram(){
+    console.log("Closing Connection");
+    connection.end();
+    process.exit(0);
+};
+
+function displayInventory(){
+    connection.query("SELECT * FROM bamazon")
+    , function(err, res) {
+        if (err) throw err;
+        console.log(res);
+    } 
+};
